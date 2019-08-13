a1a6ed169f0082504ba32bdd7f1ce57d448037c2
Onojimi <18811505578@163.com>
2019-08-13 14:06:49 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -19,6 +19,7 @@ def eval_net(net, dataset, gpu = False):
     for i, samp in enumerate(dataset):
         image = np.array(samp['image'])
         mask = np.array(samp['mask'])
+        print(image.shape)
         
         image = torch.from_numpy(image).type(torch.FloatTensor)
         mask = torch.from_numpy(mask).type(torch.FloatTensor)
