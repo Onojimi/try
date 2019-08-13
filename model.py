e2bdd6581d4b76332cf65e75d270a86045fee35b
Onojimi <18811505578@163.com>
2019-08-13 13:41:35 -0400

    Update model.py

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ def up_pooling(in_channels, out_channels, kernel_size = 2, stride = 2):
         nn.ReLU(inplace = True)
         )
 class UNet(nn.Module):
-    def __init__(self, in_ch, n_classes):
+    def __init__(self, input_channels, n_classes):
         super.__init__()
         #go down
         self.conv1 = conv_twice(in_ch, 64)
