7e36b8f603b0ec2c2224075f39f7ca0db2e93a59
Onojimi <18811505578@163.com>
2019-08-14 10:01:50 -0400

    Update combineloss.py

--- combineloss.py
+++ combineloss.py
@@ -13,6 +13,7 @@ class mixloss(nn.Module):
         
         dice_loss = torch.zeros(0, dtype = torch.float32)
         dice_loss += (2 * inter.float() + self.eps) / union.float()
+        print(pred_mask, true_mask)
         bce_loss = nn.BCELoss(pred_mask, true_mask)
         
         mix_loss = bce_loss + (1 - dice_loss)
