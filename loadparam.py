500d761086ba0ce197e63794e6d322d2f68bbc95
Onojimi <18811505578@163.com>
2019-08-15 12:44:22 -0400

    Create loadparam.py

---
+++ loadparam.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch import optim
+from model import UNet
+from torchvision import models
+
+model = UNet()
+model = nn.DataParallel(model)
+model_dict = model.state_dict()
+pretrained_dict = torch.load('CP50.pth')
+pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+
+model_dict.update(pretrained_dict)
+
+model.load_state_dict(model_dict)
+
+for k,v in model.named_parameters():
+
+    print(k)
